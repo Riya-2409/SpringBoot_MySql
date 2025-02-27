038ff6c4f57aaf1650c134f5613066ca277e9755
Riya <soniriya2409@gmail.com>
2025-02-27 22:42:55 +0530

    [Riya] Add. Retrive All Records of Employee Payroll

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeeaayrollapp/repository/EmployeeRepository.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeeaayrollapp/repository/EmployeeRepository.java
@@ -1,10 +1,18 @@
 package com.bridgelabz.employeeaayrollapp.repository;
 
-
 import com.bridgelabz.employeeaayrollapp.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query(value = "SELECT * FROM employee_payroll ep " +
+            "JOIN employee_department ed ON ep.employee_id = ed.id " +
+            "WHERE ed.department = :department", nativeQuery = true)
+    List<Employee> findEmployeesByDepartment(String department);
 }
